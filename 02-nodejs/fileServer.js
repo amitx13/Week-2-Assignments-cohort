77480c0ea15b0049c0c37d5404a5fa3c593e624b
amitk <amitk@amitx13>
2023-11-27 22:39:57 +0530

    added the second assignment

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -20,6 +20,33 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+/* const port = 3000; */
+
+app.get('/files', (req, res) => {
+  fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' });
+    }
+    res.json(files);
+  });
+});
+
+app.get('/files/:filename',(req,res)=>{
+  const filepath = path.join(__dirname ,'./files/',req.params.filename)
+  fs.readFile(`${filepath}`,'utf8',(err,data)=>{
+    if (err) {
+      return res.status(404).send('File not found');
+    }
+    res.send(data);
+  });
+})
 
 
+app.use((req, res)=>{
+  res.status(404).send('Route not found');
+})
+
+/* app.listen(port,()=>{
+
+}) */
 module.exports = app;
